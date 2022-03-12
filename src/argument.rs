9035c66c7bb798787c1d00a44f00b2ad4699b406
Wuriyanto <wuriyanto48@yahoo.co.id>
2022-03-12 15:51:43 +0700

    add custom output folder

--- src/argument.rs
+++ src/argument.rs
@@ -37,19 +37,47 @@ impl Argument {
                 flag_param.contains("-version") && args.len() == 2 {
                 argument.version = true;
             } else {
+                let mut out_path = String::new();
+                if args.len() == 4 {
+                    let output_flag = args[2].clone();
+                    let output_path = args[3].clone();
+                    if output_flag.contains("-o") || 
+                        output_flag.contains("--output") {
+                            if let Ok(md) = std::fs::metadata(output_path.clone()) {
+                                if md.is_dir() {
+                                    out_path = output_path;
+                                } else {
+                                    argument.error = true;
+                                    argument.show_error = Box::new(|| { 
+                                        println!("error parsing output path: path is not a directory"); 
+                                    });
+                                }
+                            } else {
+                                argument.error = true;
+                                argument.show_error = Box::new(|| { 
+                                    println!("error parsing output path: path is invalid"); 
+                                });
+                            }
+                    }
+                }
+
+                let base_path = std::path::Path::new(&out_path);
+
                 if let Ok(mut parsed_url) = Url::parse(&flag_param) {
                     parsed_url.query_pairs_mut().append_pair("__a", "1");
+                    
                     if parsed_url.path_segments().is_none() {
-                        argument.unique_path = Some(String::from("out"));
+                        argument.unique_path = Some(String::from(base_path.join("out").to_str().unwrap()));
                     } else {
                         let path_segments = parsed_url.path_segments().unwrap();
                         let path_vec: Vec<&str> = path_segments.collect();
                         if path_vec.get(1).is_none() {
-                            argument.unique_path = Some(String::from("out"));
+                            argument.unique_path = Some(String::from(base_path.join("out").to_str().unwrap()));
                         } else {
-                            argument.unique_path = Some((*path_vec.get(1).unwrap()).to_string());
+                            argument.unique_path = Some(String::from(base_path.join((*path_vec.get(1).unwrap()).to_string()).to_str().unwrap()));
                         }
                     }
+
                     argument.url = Some(parsed_url);
                 } else {
                     argument.error = true;
@@ -70,8 +98,18 @@ fn show_help() {
     println!();
     println!("> instaget https://instagram.com/p/BlaBla");
     println!();
-    println!("> -h/ --help: show help");
-    println!("> -v/ --version: show version");
+    println!();
+    println!("> instaget https://instagram.com/p/BlaBla --output /Users/johndoe/folder");
+    println!();
+    println!("----------- flag options: -----------");
+    println!();
+    println!("> -o | --output: set custom output folder");
+    println!("> -h | --help: show help");
+    println!("> -v | --version: show version");
+    println!();
+    println!("---------------------------------------");
+    println!();
+    show_version();
     println!();
     println!("---------------------------------------");
 }
