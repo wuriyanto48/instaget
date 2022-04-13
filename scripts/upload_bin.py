c17d19c203e4669f4bca7bd41e620a3552a8e29f
Wuriyanto <wuriyanto48@yahoo.co.id>
2022-04-13 20:14:04 +0700

    upload script: set storage client as a upload function param

--- scripts/upload_bin.py
+++ scripts/upload_bin.py
@@ -5,11 +5,11 @@ from minio import Minio
 
 BUCKET_NAME = 'instaget'
 
-def upload(object_name: str, bin_path: str):
+def upload(storage_client: Minio, object_name: str, bin_path: str):
     file_stat = os.stat(bin_path)
     with open(bin_path, 'rb') as buff:
-        client.remove_object(bucket_name=BUCKET_NAME, object_name=object_name)
-        result = client.put_object(bucket_name=BUCKET_NAME, object_name=object_name, data=buff, length=file_stat.st_size)
+        storage_client.remove_object(bucket_name=BUCKET_NAME, object_name=object_name)
+        result = storage_client.put_object(bucket_name=BUCKET_NAME, object_name=object_name, data=buff, length=file_stat.st_size)
         print('upload result ', result.object_name)
 
 if __name__ == '__main__':
@@ -31,11 +31,11 @@ if __name__ == '__main__':
         print('uploading osx bin')
         object_name = 'osx/instaget'
         osx_bin_path = os.path.join(cur_dir, 'build-result/osx/x86_64-apple-darwin/debug/instaget')
-        upload(object_name=object_name, bin_path=osx_bin_path)
+        upload(client, object_name=object_name, bin_path=osx_bin_path)
         print('uploading osx bin succeed.....')
     else:
         print('uploading linux bin')
         object_name = 'linux/instaget'
         linux_bin_path = os.path.join(cur_dir, 'build-result/linux/x86_64-unknown-linux-gnu/debug/instaget')
-        upload(object_name=object_name, bin_path=linux_bin_path)
+        upload(client, object_name=object_name, bin_path=linux_bin_path)
         print('uploading linux bin succeed.....')
